8b5392cd87f19d200da68143f0ce81676b1733f9
Bruno Batista <bruno@onefootprint.com>
2023-10-16 11:42:32 +0200

    Update cleave-angular.js

--- dist/cleave-angular.js
+++ dist/cleave-angular.js
@@ -1360,7 +1360,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	    },
 
 	    getPostDelimiter: function (value, delimiter, delimiters) {
-			if (!value) return '';
+		if (!value) return '';
 	        // single delimiter
 	        if (delimiters.length === 0) {
 	            return value.slice(-delimiter.length) === delimiter ? delimiter : '';
